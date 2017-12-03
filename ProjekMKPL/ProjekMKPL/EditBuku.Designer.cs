dfaa5e5faf2089b6e40f15f82a4f459a2c2432ba
awns-ida <siti.wahidah96@gmail.com>
2017-12-03 12:13:13 +0700

    edit form tambah dan update buku

--- ProjekMKPL/ProjekMKPL/EditBuku.Designer.cs
+++ ProjekMKPL/ProjekMKPL/EditBuku.Designer.cs
@@ -28,14 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rbDeskripsi = new System.Windows.Forms.RichTextBox();
+            this.btBatal = new System.Windows.Forms.Button();
+            this.btSimpan = new System.Windows.Forms.Button();
+            this.tbNomorSeri = new System.Windows.Forms.TextBox();
+            this.tbTahunTerbit = new System.Windows.Forms.TextBox();
+            this.tbPengarang = new System.Windows.Forms.TextBox();
+            this.tbPenerbit = new System.Windows.Forms.TextBox();
+            this.tbJudul = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -44,124 +44,125 @@
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button2
+            // rbDeskripsi
             // 
-            this.button2.Location = new System.Drawing.Point(205, 266);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 45;
-            this.button2.Text = "Batal";
-            this.button2.UseVisualStyleBackColor = true;
+            this.rbDeskripsi.Location = new System.Drawing.Point(159, 183);
+            this.rbDeskripsi.Name = "rbDeskripsi";
+            this.rbDeskripsi.Size = new System.Drawing.Size(245, 73);
+            this.rbDeskripsi.TabIndex = 46;
+            this.rbDeskripsi.Text = "";
             // 
-            // button1
+            // btBatal
             // 
-            this.button1.Location = new System.Drawing.Point(329, 266);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 44;
-            this.button1.Text = "Simpan";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btBatal.Location = new System.Drawing.Point(159, 270);
+            this.btBatal.Name = "btBatal";
+            this.btBatal.Size = new System.Drawing.Size(75, 23);
+            this.btBatal.TabIndex = 45;
+            this.btBatal.Text = "Batal";
+            this.btBatal.UseVisualStyleBackColor = true;
             // 
-            // textBox6
+            // btSimpan
             // 
-            this.textBox6.Location = new System.Drawing.Point(159, 184);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(245, 20);
-            this.textBox6.TabIndex = 43;
+            this.btSimpan.Location = new System.Drawing.Point(329, 270);
+            this.btSimpan.Name = "btSimpan";
+            this.btSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btSimpan.TabIndex = 44;
+            this.btSimpan.Text = "Simpan";
+            this.btSimpan.UseVisualStyleBackColor = true;
             // 
-            // textBox5
+            // tbNomorSeri
             // 
-            this.textBox5.Location = new System.Drawing.Point(159, 152);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(245, 20);
-            this.textBox5.TabIndex = 42;
+            this.tbNomorSeri.Location = new System.Drawing.Point(159, 148);
+            this.tbNomorSeri.Name = "tbNomorSeri";
+            this.tbNomorSeri.Size = new System.Drawing.Size(245, 20);
+            this.tbNomorSeri.TabIndex = 43;
             // 
-            // textBox4
+            // tbTahunTerbit
             // 
-            this.textBox4.Location = new System.Drawing.Point(159, 123);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(245, 20);
-            this.textBox4.TabIndex = 41;
+            this.tbTahunTerbit.Location = new System.Drawing.Point(159, 119);
+            this.tbTahunTerbit.Name = "tbTahunTerbit";
+            this.tbTahunTerbit.Size = new System.Drawing.Size(245, 20);
+            this.tbTahunTerbit.TabIndex = 42;
             // 
-            // textBox3
+            // tbPengarang
             // 
-            this.textBox3.Location = new System.Drawing.Point(159, 94);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(245, 20);
-            this.textBox3.TabIndex = 40;
+            this.tbPengarang.Location = new System.Drawing.Point(159, 90);
+            this.tbPengarang.Name = "tbPengarang";
+            this.tbPengarang.Size = new System.Drawing.Size(245, 20);
+            this.tbPengarang.TabIndex = 41;
             // 
-            // textBox2
+            // tbPenerbit
             // 
-            this.textBox2.Location = new System.Drawing.Point(159, 63);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(245, 20);
-            this.textBox2.TabIndex = 39;
+            this.tbPenerbit.Location = new System.Drawing.Point(159, 59);
+            this.tbPenerbit.Name = "tbPenerbit";
+            this.tbPenerbit.Size = new System.Drawing.Size(245, 20);
+            this.tbPenerbit.TabIndex = 40;
             // 
-            // textBox1
+            // tbJudul
             // 
-            this.textBox1.Location = new System.Drawing.Point(159, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(245, 20);
-            this.textBox1.TabIndex = 38;
+            this.tbJudul.Location = new System.Drawing.Point(159, 29);
+            this.tbJudul.Name = "tbJudul";
+            this.tbJudul.Size = new System.Drawing.Size(245, 20);
+            this.tbJudul.TabIndex = 39;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(56, 186);
+            this.label6.Location = new System.Drawing.Point(56, 182);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(70, 18);
-            this.label6.TabIndex = 37;
+            this.label6.TabIndex = 38;
             this.label6.Text = "Deskripsi";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(56, 154);
+            this.label5.Location = new System.Drawing.Point(56, 150);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(85, 18);
-            this.label5.TabIndex = 36;
+            this.label5.TabIndex = 37;
             this.label5.Text = "Nomor Seri";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(56, 125);
+            this.label4.Location = new System.Drawing.Point(56, 121);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 18);
-            this.label4.TabIndex = 35;
+            this.label4.TabIndex = 36;
             this.label4.Text = "Tahun Terbit";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(56, 96);
+            this.label3.Location = new System.Drawing.Point(56, 92);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 18);
-            this.label3.TabIndex = 34;
+            this.label3.TabIndex = 35;
             this.label3.Text = "Pengarang";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(56, 65);
+            this.label2.Location = new System.Drawing.Point(56, 61);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 18);
-            this.label2.TabIndex = 33;
+            this.label2.TabIndex = 34;
             this.label2.Text = "Penerbit";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(56, 35);
+            this.label1.Location = new System.Drawing.Point(56, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 18);
-            this.label1.TabIndex = 32;
+            this.label1.TabIndex = 33;
             this.label1.Text = "Judul";
             // 
             // EditBuku
@@ -169,14 +170,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(460, 323);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rbDeskripsi);
+            this.Controls.Add(this.btBatal);
+            this.Controls.Add(this.btSimpan);
+            this.Controls.Add(this.tbNomorSeri);
+            this.Controls.Add(this.tbTahunTerbit);
+            this.Controls.Add(this.tbPengarang);
+            this.Controls.Add(this.tbPenerbit);
+            this.Controls.Add(this.tbJudul);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -192,19 +193,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rbDeskripsi;
+        private System.Windows.Forms.Button btBatal;
+        private System.Windows.Forms.Button btSimpan;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbJudul;
+        private System.Windows.Forms.TextBox tbPenerbit;
+        private System.Windows.Forms.TextBox tbPengarang;
+        private System.Windows.Forms.TextBox tbTahunTerbit;
+        private System.Windows.Forms.TextBox tbNomorSeri;
     }
 }
